7f2d6d9bf75eaed849e8b22ea6b12d6946f065b9
unknown <blurpek@protonmail.com>
2018-11-13 14:25:40 +0100

    balance gameplay

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ let game = {
 };
 
 const config = {
-    baseIncome: 0.5,
-    baseIncomeByClick: 2,
+    baseIncome: 0.7,
+    baseIncomeByClick: 2.5,
     baseIncomePrice: 100,
     baseClickerPrice: 50,
-    mulltiplier: 1.15,
+    mulltiplier: 1.25,
     loopInterval: 180,  // 33 milliseconds = ~ 30 frames per sec
     saveInterval: 1000,
-    incomeFromBackgroungMultiplier: 0.6
+    incomeFromBackgroungMultiplier: 0.7
 }
 
 const drawable = {
